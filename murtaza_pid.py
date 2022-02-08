202b571839aea2e283114dc175e043f6cf1371ff
Ilgar <gragli98@gmail.com>
2022-02-08 13:18:51 -0500

    started applying about pid

---
+++ murtaza_pid.py
@@ -0,0 +1,17 @@
+from murtaza_pid_utils import *
+import cv2
+
+w,h = 360, 240
+
+myDrone=initializeTello()
+
+while True:
+
+	#Step1
+	img=telloGetFrame(myDrone,w,h)
+	cv2.imshow('Image',img)
+
+	if cv2.waitKey(1) & 0xFF==ord('q'):
+		myDrone.land()
+		break
+
